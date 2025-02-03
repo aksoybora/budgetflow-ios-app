d542cfdc9a96f08b9b35b5cdaa26b2639587b5ab
Bora Aksoy <bora.aaksoy@gmail.com>
2025-02-03 23:27:26 +0300

    feat: Initial project setup with Firebase Authentication and Tab Bar
    - Implemented Firebase authentication (Sign in, Sign up, Sign out)
    - Added Tab Bar with Home, Transactions, Reports, and Settings screens
    - Basic UI layout for main sections

---
+++ BudgetFlow/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  BudgetFlow
+//
+//  Created by Bora Aksoy on 3.02.2025.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logOutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            performSegue(withIdentifier: "toVC", sender: nil)
+        } catch {
+            print("Sign Out failed!")
+        }
+    }
+    
+    
+}
--- BudgetFlow/ViewController.swift
+++ BudgetFlow/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toHomeVC", sender: nil)
+                }
+            }
+            
+        } else {
+            self.makeAlert(titleInput: "Error!", messageInput: "No email or password was entered.")
+        }
+    }
+    
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toHomeVC", sender: nil)
+                }
+            }
+            
+        } else {
+            self.makeAlert(titleInput: "Error!", messageInput: "No email or password was entered.")
+        }
+    }
+    
+    
+    
+    func makeAlert(titleInput:String, messageInput:String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let OKButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(OKButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
